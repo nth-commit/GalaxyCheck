a678c7368c542bd1e86766d9d80c9c6d471cfefb
nth-commit <mfrywhite@gmail.com>
2021-02-22 22:02:47 +1300

    🌍 #1 Set up new test project, so that we can test ourselves silly

---
+++ tests/GalaxyCheck.Tests.V2/RandomTests/AboutCreate.cs
@@ -0,0 +1,44 @@
+﻿using NebulaCheck;
+using Xunit;
+using Rng = GalaxyCheck.Internal.Random.Rng;
+
+namespace Tests.V2.RandomTests
+{
+    public class AboutCreate
+    {
+        [Fact]
+        public void ItIsRepeatable()
+        {
+            var checkResult = Gen
+                .Int32()
+                .NoShrink()
+                .ForAll(seed =>
+                {
+                    var rng0 = Rng.Create(seed);
+                    var rng1 = Rng.Create(seed);
+
+                    Assert.Equal(rng0, rng1);
+                })
+                .Check();
+
+            Assert.False(checkResult.Falsified);
+        }
+
+        [Fact]
+        public void ItInitializesTheOrder()
+        {
+            var checkResult = Gen
+                .Int32()
+                .NoShrink()
+                .ForAll(seed =>
+                {
+                    var rng = Rng.Create(seed);
+
+                    Assert.Equal(0, rng.Order);
+                })
+                .Check();
+
+            Assert.False(checkResult.Falsified);
+        }
+    }
+}
