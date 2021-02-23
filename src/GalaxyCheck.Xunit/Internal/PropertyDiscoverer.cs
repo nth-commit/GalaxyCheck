c50017409df160128a2e624c95e69d0cb41584be
Michael Fry <mfrywhite@gmail.com>
2021-02-23 21:48:12 +1300

    🌍 #6 Bootstrap basic GalaxyCheck.Xunit project, with integration tests (#7)

---
+++ src/GalaxyCheck.Xunit/Internal/PropertyDiscoverer.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections.Generic;
+using Xunit.Abstractions;
+using Xunit.Sdk;
+
+namespace GalaxyCheck.Xunit.Internal
+{
+    public class PropertyDiscoverer : IXunitTestCaseDiscoverer
+    {
+        private readonly IMessageSink _messageSink;
+
+        public PropertyDiscoverer(IMessageSink messageSink)
+        {
+            _messageSink = messageSink;
+        }
+
+        public IEnumerable<IXunitTestCase> Discover(
+            ITestFrameworkDiscoveryOptions discoveryOptions,
+            ITestMethod testMethod,
+            IAttributeInfo factAttribute)
+        {
+            yield return new PropertyTestCase(
+                _messageSink,
+                discoveryOptions.MethodDisplayOrDefault(),
+                discoveryOptions.MethodDisplayOptionsOrDefault(),
+                testMethod);
+        }
+    }
+}
---
+++ src/GalaxyCheck.Xunit/PropertyAttribute.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using Xunit;
+using Xunit.Sdk;
+
+namespace GalaxyCheck.Xunit
+{
+    [AttributeUsage(AttributeTargets.Method, AllowMultiple = false)]
+    [XunitTestCaseDiscoverer("GalaxyCheck.Xunit.Internal.PropertyDiscoverer", "GalaxyCheck.Xunit")]
+    public class PropertyAttribute : FactAttribute
+    {
+    }
+}
