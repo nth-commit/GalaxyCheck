2da5540bec4291a8a34f060816e506476770a2a6
Michael Fry <mfrywhite@gmail.com>
2021-02-25 09:17:13 +1300

    🌍 #11 Basic implementation of converting a MethodInfo to a property (#12)
    Taking a reflected method we can convert it to a property and check it, using generators as input and then evaluating the output.
    
    Only adds supports for syncrhonous void and boolean properties.

---
+++ tests/GalaxyCheck.Tests.V2/GenAssert.cs
@@ -0,0 +1,45 @@
+﻿using GalaxyCheck;
+using GalaxyCheck.Internal.ExampleSpaces;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace Tests.V2
+{
+    public static class GenAssert
+    {
+        public static void Equal<T>(IGen<T> expected, IGen<T> actual, int seed, int? iterations = null)
+        {
+            var expectedSample = expected.Select(x => JsonConvert.SerializeObject(x)).Advanced.SampleExampleSpaces(seed: seed, iterations: iterations);
+            var actualSample = actual.Select(x => JsonConvert.SerializeObject(x)).Advanced.SampleExampleSpaces(seed: seed, iterations: iterations);
+
+            Assert.All(
+                Enumerable.Zip(expectedSample, actualSample),
+                (x) =>
+                {
+                    Assert.Equal(x.First.Sample(), x.Second.Sample());
+                });
+        }
+
+        public static List<Example<T>> Sample<T>(
+            this ExampleSpace<T> exampleSpace,
+            int maxExamples = 10)
+        {
+            static IEnumerable<Example<T>> SampleRec(ExampleSpace<T> exampleSpace)
+            {
+                yield return exampleSpace.Current;
+
+                foreach (var subExampleSpace in exampleSpace.Subspace.SelectMany(es => SampleRec(es)))
+                {
+                    yield return subExampleSpace;
+                }
+            }
+
+            return SampleRec(exampleSpace).Take(maxExamples).ToList();
+        }
+    }
+}
