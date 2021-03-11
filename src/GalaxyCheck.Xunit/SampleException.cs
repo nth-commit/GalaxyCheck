692c54d8ed6e886ec6037b13d22ca238b9ef3341
nth-commit <mfrywhite@gmail.com>
2021-03-11 18:58:02 +1300

    🌍 #29 Extract the [Sample] formatting logic to Print, add snapshots

--- src/GalaxyCheck.Xunit/SampleException.cs
+++ src/GalaxyCheck.Xunit/SampleException.cs
@@ -8,41 +8,6 @@ namespace GalaxyCheck.Xunit
 {
     public class SampleException : Exception
     {
-        public SampleException(SampleWithMetricsResult<PresentableExampleSpace<Test<object>>> sample) : base(BuildMessage(sample)) { }
-
-        private static string BuildMessage(SampleWithMetricsResult<PresentableExampleSpace<Test<object>>> sample) => string.Join(Environment.NewLine, BuildLines(sample));
-
-        private static IEnumerable<string> BuildLines(SampleWithMetricsResult<PresentableExampleSpace<Test<object>>> sample)
-        {
-            const string LineBreak = "";
-
-            yield return "Test case failed to prevent false-positives.";
-
-            yield return LineBreak;
-
-            yield return $"Sampled {sample.Values.Count} values ({sample.Discards} discarded):";
-
-            for (var i = 0; i < sample.Values.Count; i++)
-            {
-                var value = sample.Values[i];
-
-                var example = ExampleViewModel.Infer(
-                    value.Presentational?.Current.Value ??
-                    value.Actual.Current.Value.Input);
-
-                var lines = ExampleRenderer.Render(example).ToList();
-
-                yield return lines.Count switch
-                {
-                    0 => throw new Exception("Fatal: Expected at least one line to be rendered in counterexample"),
-                    1 => $"Sample[{i}]: {lines.Single()}",
-                    _ => string.Join(
-                        Environment.NewLine,
-                        Enumerable.Concat(
-                            new[] { LineBreak, $"Sample[{i}]:" },
-                            lines.Select(l => $"    {l}")))
-                };
-            }
-        }
+        public SampleException(string message) : base($"Test case failed to prevent false-positives.{Environment.NewLine}{Environment.NewLine}{message}") { }
     }
 }
---
+++ tests/GalaxyCheck.Tests.V2/RunnerTests/PrintTests/Snapshots.cs
@@ -0,0 +1,114 @@
+﻿using GalaxyCheck;
+using Snapshooter.Xunit;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using Xunit;
+
+namespace Tests.V2.RunnerTests.PrintTests
+{
+    public class Snapshots
+    {
+        [Fact]
+        public void PrintInt32Gen()
+        {
+            var gen = Gen.Int32();
+
+            var print = PrintAndCollect(gen.Cast<object>());
+
+            Snapshot.Match(print);
+        }
+
+        [Fact]
+        public void PrintInt32GenFiltered()
+        {
+            var gen = Gen.Int32().Where(x => x % 2 == 0);
+
+            var print = PrintAndCollect(gen.Cast<object>());
+
+            Snapshot.Match(print);
+        }
+
+        [Fact]
+        public void PrintListOfInt32Gen()
+        {
+            var gen = Gen.Int32().ListOf();
+
+            var print = PrintAndCollect(gen);
+
+            Snapshot.Match(print);
+        }
+
+        [Fact]
+        public void PrintNullaryProperty()
+        {
+            // TODO: This test should print "(no value)", rather than literal null (as nullary properties should be
+            // rendered in this way. This is avoiding that rendering path, which isn't a big deal, but could cause
+            // other issues / is a sign of hackiness.
+
+            var property = Property.Nullary(() => false);
+
+            var print = PrintAndCollect(property, enableLinqInference: true);
+
+            Snapshot.Match(print);
+        }
+
+        [Fact]
+        public void PrintUnaryProperty()
+        {
+            var property = Gen.Int32().ForAll((_) => false);
+
+            var print = PrintAndCollect(property, enableLinqInference: true);
+
+            Snapshot.Match(print);
+        }
+
+        [Fact]
+        public void PrintBinaryProperty()
+        {
+            var property =
+                from x in Gen.Int32()
+                from y in Gen.Int32().GreaterThan(x)
+                select Property.ForThese(() => false);
+
+            var print = PrintAndCollect(property, enableLinqInference: true);
+
+            Snapshot.Match(print);
+        }
+
+        [Fact]
+        public void PrintUnaryListProperty()
+        {
+            var property = Gen.Int32().ListOf().ForAll((_) => false);
+
+            var print = PrintAndCollect(property, enableLinqInference: true);
+
+            Snapshot.Match(print);
+        }
+
+        [Fact]
+        public void PrintBinaryListProperty()
+        {
+            var property =
+                from xs in Gen.Int32().ListOf()
+                from ys in Gen.Int32().ListOf().WithCountGreaterThan(xs.Count)
+                select Property.ForThese(() => false);
+
+            var print = PrintAndCollect(property, enableLinqInference: true);
+
+            Snapshot.Match(print);
+        }
+
+        private static string PrintAndCollect(IGen<object> gen, bool enableLinqInference = false)
+        {
+            var log = new List<string>();
+
+            gen.Advanced.Print(
+                seed: 0,
+                stdout: log.Add,
+                enableLinqInference: enableLinqInference);
+
+            return string.Join(Environment.NewLine, log);
+        }
+    }
+}
