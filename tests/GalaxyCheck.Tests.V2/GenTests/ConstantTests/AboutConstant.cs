c4ff02636fb1d25b448a0c56837b471e978da32b
nth-commit <mfrywhite@gmail.com>
2021-03-13 12:27:38 +1300

    🌍 #38 Migrate constant tests

---
+++ tests/GalaxyCheck.Tests.V2/GenTests/ConstantTests/AboutConstant.cs
@@ -0,0 +1,47 @@
+﻿using FluentAssertions;
+using GalaxyCheck;
+using NebulaCheck;
+using NebulaCheck.Xunit;
+using System.Linq;
+using Property = NebulaCheck.Property;
+using Test = NebulaCheck.Test;
+
+namespace Tests.V2.GenTests.ConstantTests
+{
+    public class AboutConstant
+    {
+        [Property]
+        public NebulaCheck.IGen<Test> ItProducesTheGivenValue() =>
+            from value in DomainGen.Any()
+            from seed in DomainGen.Seed()
+            from size in DomainGen.Size()
+            select Property.ForThese(() =>
+            {
+                var gen = GalaxyCheck.Gen.Constant(value);
+
+                var traversal = gen.Advanced
+                    .SampleOneExampleSpace(seed: seed, size: size)
+                    .Traverse()
+                    .Take(100)
+                    .ToList();
+
+                traversal
+                    .Should().ContainSingle()
+                    .Subject.Should().BeEquivalentTo(value);
+            });
+
+        [Property]
+        public NebulaCheck.IGen<Test> ItDoesNotConsumeRandomness() =>
+            from value in DomainGen.Any()
+            from seed in DomainGen.Seed()
+            from size in DomainGen.Size()
+            select Property.ForThese(() =>
+            {
+                var gen = GalaxyCheck.Gen.Constant(value);
+
+                var sample = gen.Advanced.SampleOneWithMetrics(seed: seed, size: size);
+
+                sample.RandomnessConsumption.Should().Be(0);
+            });
+    }
+}
