a0655df3e2cb8cc83e41151781ffe2684ccd14d0
Michael Fry <mfrywhite@gmail.com>
2021-03-14 07:23:13 +1300

    🌍 #78 Add encoder for replay recipe (#79)

---
+++ src/GalaxyCheck/Internal/Replaying/Replay.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections.Generic;
+
+namespace GalaxyCheck.Internal.Replaying
+{
+    public record Replay(
+        int Seed,
+        int Size,
+        IEnumerable<int> ExampleSpacePath);
+}
