6f1d68e48155b05863a597226838203ea75152d7
nth-commit <mfrywhite@gmail.com>
2021-03-15 20:02:00 +1300

    🌍 #87 Prototype: A hook for contextualized shrinking
    This should let us make smarter decisions about shrinking, by observing what values came before. This part of the lib needs a tidy at some point. But this is just a prototype so meh.

--- src/GalaxyCheck/Internal/ExampleSpaces/ExampleSpaceFactory.cs
+++ src/GalaxyCheck/Internal/ExampleSpaces/ExampleSpaceFactory.cs
@@ -1,12 +1,11 @@
-﻿using GalaxyCheck.Internal.Utility;
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.Collections.Immutable;
 using System.Linq;
 
 namespace GalaxyCheck.Internal.ExampleSpaces
 {
-    public static class ExampleSpaceFactory
+    public static partial class ExampleSpaceFactory
     {
         private record ExampleSpace<T> : IExampleSpace<T>
         {
@@ -40,31 +39,6 @@ namespace GalaxyCheck.Internal.ExampleSpaces
             new Example<T>(id, value, 0),
             Enumerable.Empty<IExampleSpace<T>>());
 
-        /// <summary>
-        /// Creates an example space by recursively applying a shrinking function to the root value.
-        /// </summary>
-        /// <typeparam name="T">The type of the example's value.</typeparam>
-        /// <param name="rootValue"></param>
-        /// <param name="shrink"></param>
-        /// <param name="measure"></param>
-        /// <param name="identify"></param>
-        /// <returns></returns>
-        public static IExampleSpace<T> Unfold<T>(
-            T rootValue,
-            ShrinkFunc<T> shrink,
-            MeasureFunc<T> measure,
-            IdentifyFunc<T> identify)
-        {
-            var id = identify(rootValue);
-            return UnfoldInternal(
-                rootValue,
-                shrink,
-                measure,
-                identify,
-                x => x,
-                ImmutableHashSet.Create(id));
-        }
-
         public static IExampleSpace<T> Delay<T>(
             IExample<T> rootExample,
             Func<IEnumerable<IExampleSpace<T>>> delayedSubspace)
@@ -78,37 +52,6 @@ namespace GalaxyCheck.Internal.ExampleSpaces
             }
 
             return new ExampleSpace<T>(rootExample, DelayEnumeration(delayedSubspace));
-        }
-
-        private static ExampleSpace<TProjection> UnfoldInternal<TAccumulator, TProjection>(
-            TAccumulator accumulator,
-            ShrinkFunc<TAccumulator> shrink,
-            MeasureFunc<TAccumulator> measure,
-            IdentifyFunc<TAccumulator> identify,
-            Func<TAccumulator, TProjection> projection,
-            ImmutableHashSet<ExampleId> encountered)
-        {
-            var id = identify(accumulator);
-            var value = projection(accumulator);
-            var distance = measure(accumulator);
-            return new ExampleSpace<TProjection>(
-                new Example<TProjection>(id, value, distance),
-                UnfoldSubspaceInternal(shrink(accumulator), shrink, measure, identify, projection, encountered));
-        }
-
-        private static IEnumerable<IExampleSpace<TProjection>> UnfoldSubspaceInternal<TAccumulator, TProjection>(
-            IEnumerable<TAccumulator> accumulators,
-            ShrinkFunc<TAccumulator> shrink,
-            MeasureFunc<TAccumulator> measure,
-            IdentifyFunc<TAccumulator> identify,
-            Func<TAccumulator, TProjection> projection,
-            ImmutableHashSet<ExampleId> encountered)
-        {
-            return TraverseUnencountered(
-                accumulators,
-                (accumulator, encountered) =>
-                    UnfoldInternal(accumulator, shrink, measure, identify, projection, encountered),
-                encountered);
         }
 
         public static IExampleSpace<TResult> Merge<T, TResult>(
@@ -130,6 +73,13 @@ namespace GalaxyCheck.Internal.ExampleSpaces
             MeasureFunc<List<IExampleSpace<T>>> measureMerge,
             ImmutableHashSet<ExampleId> encountered)
         {
+            IExampleSpace<TResult> GenerateNextExampleSpace(
+                IEnumerable<IExampleSpace<T>> exampleSpaces,
+                ImmutableHashSet<ExampleId> encountered)
+            {
+                return MergeInternal(exampleSpaces.ToList(), mergeValues, shrinkExampleSpaces, measureMerge, encountered);
+            }
+
             var mergedId = exampleSpaces.Aggregate(
                 ExampleId.Primitive(exampleSpaces.Count()),
                 (acc, curr) => ExampleId.Combine(acc, curr.Current.Id));
@@ -151,9 +101,8 @@ namespace GalaxyCheck.Internal.ExampleSpaces
 
             var shrinks = TraverseUnencountered(
                 Enumerable.Concat(exampleSpaceCullingShrinks, subspaceMergingShrinks),
-                (exampleSpaces0, encountered0) =>
-                    MergeInternal(exampleSpaces0.ToList(), mergeValues, shrinkExampleSpaces, measureMerge, encountered0),
-                encountered);
+                encountered,
+                GenerateNextExampleSpace);
 
             return new ExampleSpace<TResult>(current, shrinks);
         }
@@ -172,60 +121,30 @@ namespace GalaxyCheck.Internal.ExampleSpaces
                     Enumerable.Concat(new[] { exampleSpace }, rightExampleSpaces)));
         }
 
-        private static IEnumerable<IExampleSpace<T>> TraverseUnencountered<TAccumulator, T>(
-            IEnumerable<TAccumulator> accumulators,
-            Func<TAccumulator, ImmutableHashSet<ExampleId>, IExampleSpace<T>> generateNextExampleSpace,
-            ImmutableHashSet<ExampleId> encountered)
-        {
-            return accumulators
-                .Scan(
-                    new UnfoldSubspaceState<IExampleSpace<T>>(
-                        new Option.None<IExampleSpace<T>>(),
-                        encountered),
-                    (acc, curr) =>
-                    {
-                        var exampleSpace = generateNextExampleSpace(curr, acc.Encountered);
-
-                        var hasBeenEncountered = acc.Encountered.Contains(exampleSpace.Current.Id);
-                        if (hasBeenEncountered)
-                        {
-                            return new UnfoldSubspaceState<IExampleSpace<T>>(
-                                new Option.None<IExampleSpace<T>>(),
-                                acc.Encountered);
-                        }
-
-                        return new UnfoldSubspaceState<IExampleSpace<T>>(
-                                new Option.Some<IExampleSpace<T>>(exampleSpace),
-                                acc.Encountered.Add(exampleSpace.Current.Id));
-                    }
-                )
-                .Select(x => x.UnencounteredValue switch
-                {
-                    Option.None<IExampleSpace<T>> _ => null,
-                    Option.Some<IExampleSpace<T>> some => some.Value,
-                    _ => null
-                })
-                .Where(exampleSpace => exampleSpace != null)
-                .Cast<IExampleSpace<T>>();
-        }
-
-        private class UnfoldSubspaceState<T>
-        {
-            public Option<T> UnencounteredValue { get; init; }
-
-            public ImmutableHashSet<ExampleId> Encountered { get; init; }
-
-            public UnfoldSubspaceState(Option<T> unencounteredValue, ImmutableHashSet<ExampleId> encountered)
-            {
-                UnencounteredValue = unencounteredValue;
-                Encountered = encountered;
-            }
-        }
-
         public static IExampleSpace<int> Int32(int value, int origin, int min, int max) => Unfold(
             value,
             ShrinkFunc.Towards(origin),
             MeasureFunc.DistanceFromOrigin(origin, min, max),
             IdentifyFuncs.Default<int>());
+
+        private record Int32OptimizedContext(
+            ImmutableList<int> EncounteredValues,
+            int CurrentOrigin);
+
+        public static IExampleSpace<int> Int32Optimized(int rootValue, int rootOrigin, int min, int max) => UnfoldContextual(
+            rootValue,
+            new Int32OptimizedContext(ImmutableList.Create<int>(), rootOrigin),
+            (value, context) =>
+            {
+                return ShrinkFunc.Towards(context.CurrentOrigin)(value);
+            },
+            MeasureFunc.DistanceFromOrigin(rootOrigin, min, max),
+            IdentifyFuncs.Default<int>(),
+            (context, value) =>
+            {
+                return new Int32OptimizedContext(
+                    context.EncounteredValues.Add(value),
+                    context.CurrentOrigin);
+            });
     }
 }
