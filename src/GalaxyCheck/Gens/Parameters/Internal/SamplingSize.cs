dea75e362a2ccb67cb4d7f8b068ada258e840a17
nth-commit <mfrywhite@gmail.com>
2021-03-22 08:00:30 +1300

    🌍 #102 Add size interpolating algorithm to be used for checking less iterations

---
+++ src/GalaxyCheck/Gens/Parameters/Internal/SamplingSize.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Immutable;
+using System.Linq;
+
+namespace GalaxyCheck.Gens.Parameters.Internal
+{
+    internal class SamplingSize
+    {
+        public static ImmutableList<Size> SampleSize(int numberOfSamples)
+        {
+            var clampedNumberOfSamples = Math.Min(numberOfSamples, 101);
+            var interpolate = Interpolate(0, 0, clampedNumberOfSamples - 1, 100);
+
+            return Enumerable
+                .Range(0, clampedNumberOfSamples)
+                .Select(x => x == clampedNumberOfSamples - 1
+                    ? new Size(100)
+                    : new Size(interpolate(x)))
+                .ToImmutableList();
+        }
+
+        private static Func<int, int> Interpolate(int x1, int y1, int x2, int y2) => (x) =>
+        {
+            return (int)(y1 + (x - x1) * ((double)(y2 - y1) / (x2 - x1)));
+        };
+    }
+}
