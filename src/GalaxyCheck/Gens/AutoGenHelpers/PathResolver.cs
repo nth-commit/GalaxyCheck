21bc67ea4168874b2fffeba83062abd2cd2a99ae
Michael Fry <mfrywhite@gmail.com>
2021-03-29 08:56:37 +1300

    🌍 #138 Implement member overrides (#147)

--- src/GalaxyCheck/Gens/AutoGenHelpers/PathResolver.cs
+++ src/GalaxyCheck/Gens/AutoGenHelpers/PathResolver.cs
@@ -1,19 +1,54 @@
 ﻿using GalaxyCheck.Internal;
 using System;
+using System.Collections.Generic;
+using System.Linq;
 using System.Linq.Expressions;
 
 namespace GalaxyCheck.Gens.AutoGenHelpers
 {
-    internal record PathResolutionError(
-        string Expression,
-        string Error);
-
     internal class PathResolver
     {
-        public static Either<string, PathResolutionError> FromExpression<T, TMember>(
+        public static Either<string, string> FromExpression<T, TMember>(
             Expression<Func<T, TMember>> expression)
         {
-            return new Left<string, PathResolutionError>("$.Property");
+            var path = GetPath(expression);
+
+            if (path == null)
+            {
+                return new Right<string, string>(expression.ToString());
+            }
+
+            return new Left<string, string>($"$.{path}");
+        }
+
+        private static string? GetPath<T, TMember>(Expression<Func<T, TMember>> expr)
+        {
+            var stack = new Stack<string>();
+
+            MemberExpression? me;
+            switch (expr.Body.NodeType)
+            {
+                case ExpressionType.Convert:
+                case ExpressionType.ConvertChecked:
+                    me = ((expr.Body is UnaryExpression ue) ? ue.Operand : null) as MemberExpression;
+                    break;
+                default:
+                    me = expr.Body as MemberExpression;
+                    break;
+            }
+
+            while (me != null)
+            {
+                stack.Push(me.Member.Name);
+                me = me.Expression as MemberExpression;
+            }
+
+            if (stack.Any() == false)
+            {
+                return null;
+            }
+
+            return string.Join(".", stack.ToArray());
         }
     }
 }
