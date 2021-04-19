c6d544ad7f8059195abc1a2acb8362c7884b8d18
nth-commit <mfrywhite@gmail.com>
2021-04-19 20:59:43 +1200

    🌍 #162 Adds Gen.Enum
    Gen.Enum generates a random value of an enum.
    
    If the enum is decorated with System.FlagsAttribute, it will produce values that might be bitwise combinations of the enum values.

---
+++ tests/GalaxyCheck.Tests.V2/GenTests/EnumGenTests/AboutGeneratingFlagsEnums.cs
@@ -0,0 +1,62 @@
+﻿using FluentAssertions;
+using GalaxyCheck;
+using NebulaCheck;
+using Snapshooter;
+using Snapshooter.Xunit;
+using System;
+using System.Linq;
+using Xunit;
+using static Tests.V2.DomainGenAttributes;
+
+namespace Tests.V2.GenTests.EnumGenTests
+{
+    public class AboutGeneratingFlagsEnums
+    {
+        [Flags]
+        private enum AllowedOperations
+        {
+            Create = 1 << 0,
+
+            Read = 1 << 1,
+
+            Update = 1 << 2,
+
+            Delete = 1 << 3
+        }
+
+        [Property]
+        public void ItShrinksTowardsTheSmallestValue([Seed] int seed)
+        {
+            var gen = GalaxyCheck.Gen.Enum<AllowedOperations>();
+
+            var minimum = gen.Minimum(seed: seed);
+
+            minimum.Should().Be(AllowedOperations.Create);
+        }
+
+        [Fact]
+        public void Snapshots()
+        {
+            // Should demonstrate the combinatorial factor when generating a flags enum
+
+            var seeds = Enumerable.Range(0, 5);
+
+            var gen = GalaxyCheck.Gen.Enum<AllowedOperations>();
+
+            foreach (var seed in seeds)
+            {
+                var sample = gen
+                    .Advanced.SampleOneExampleSpace(seed: seed, size: 75)
+                    .Take(500)
+                    .Render(x => x.ToString());
+
+                var nameExtension = string.Join("_", new[]
+                {
+                    $"Seed_{seed}"
+                });
+
+                Snapshot.Match(sample, SnapshotNameExtension.Create(nameExtension));
+            }
+        }
+    }
+}
