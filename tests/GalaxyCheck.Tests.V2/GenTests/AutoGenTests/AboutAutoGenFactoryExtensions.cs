f559d5be4cbdd86933c935a7732c4553483dd903
nth-commit <mfrywhite@gmail.com>
2021-05-01 12:09:16 +1200

    🌍 #24 Add non-generic RegisterType, make generic RegisterType an extension

---
+++ tests/GalaxyCheck.Tests.V2/GenTests/AutoGenTests/AboutAutoGenFactoryExtensions.cs
@@ -0,0 +1,33 @@
+﻿using GalaxyCheck;
+using GalaxyCheck.Gens;
+using Moq;
+using System;
+using Xunit;
+
+namespace Tests.V2.GenTests.AutoGenTests
+{
+    public class AboutAutoGenFactoryExtensions
+    {
+        [Fact]
+        public void GenericRegisterTypeDefersToNonGenericRegisterType()
+        {
+            var gen = Gen.Int32();
+            var type = typeof(int);
+
+            var mockFactory = new Mock<IAutoGenFactory>();
+            mockFactory
+                .Setup(factory => factory.RegisterType(
+                    It.IsAny<Type>(),
+                    It.IsAny<IGen>()))
+                .Returns(mockFactory.Object);
+
+            mockFactory.Object.RegisterType(gen);
+
+            mockFactory.Verify(
+                factory => factory.RegisterType(
+                    It.Is<Type>(t => t == type),
+                    It.Is<IGen>(g => g == gen)),
+                Times.Once);
+        }
+    }
+}
---
+++ tests/GalaxyCheck.Tests.V2/GenTests/AutoGenTests/AboutValidation.cs
@@ -0,0 +1,25 @@
+﻿using FluentAssertions;
+using GalaxyCheck;
+using System;
+using Xunit;
+
+namespace Tests.V2.GenTests.AutoGenTests
+{
+    public class AboutValidation
+    {
+        [Fact]
+        public void ItErrorsWhenTypeOfRegisteredGeneratorIsNotAssignableToGivenType()
+        {
+            var gen = Gen
+                .AutoFactory()
+                .RegisterType(typeof(string), Gen.Int32())
+                .Create<string>();
+
+            Action test = () => gen.SampleOne();
+
+            test.Should()
+                .Throw<Exceptions.GenErrorException>()
+                .WithMessage("Error while running generator AutoGen: type 'System.Int32' was not assignable to the type it was registered to, 'System.String'");
+        }
+    }
+}
