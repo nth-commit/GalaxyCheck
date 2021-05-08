b9c4b8a514628e7709ef42ade62a930a947e30f2
nth-commit <mfrywhite@gmail.com>
2021-05-08 19:40:51 +1200

    🌍 #198 Add Gen.Guid()

---
+++ src/GalaxyCheck/Gens/GuidGen.cs
@@ -0,0 +1,20 @@
+﻿namespace GalaxyCheck
+{
+    using System.Linq;
+
+    public static partial class Gen
+    {
+        /// <summary>
+        /// Generates globally unique identifiers (<see cref="System.Guid"/>). The generation is unbiased, and the
+        /// generated guids do not shrink to simpler renditions of themselves (for example, shrinking towards
+        /// <see cref="System.Guid.Empty"/>). This is because guids should be "globally unique", we should never
+        /// generate the same guid twice. Generation is essentially the same as <see cref="System.Guid.NewGuid"/>,
+        /// except that it is controlled by the seed, so it will be stable across repeats. If you require unique guids,
+        /// even across repetitions, don't use a generator - use <see cref="System.Guid.NewGuid"/>.
+        /// </summary>
+        /// <returns>The new generator.</returns>
+        public static IGen<System.Guid> Guid() =>
+            from b in Byte().WithBias(Bias.None).ListOf().WithCount(16).NoShrink()
+            select new System.Guid(b.ToArray());
+    }
+}
