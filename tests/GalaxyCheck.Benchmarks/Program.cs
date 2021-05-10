51e1e64db643d542ba66c9a4eb28cc0dad38f76d
nth-commit <mfrywhite@gmail.com>
2021-05-10 21:23:16 +1200

    🌍 #214 Add benchmarks for Rng.Value

---
+++ tests/GalaxyCheck.Benchmarks/Program.cs
@@ -0,0 +1,12 @@
+﻿using BenchmarkDotNet.Running;
+
+namespace GalaxyCheck.Benchmarks
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            BenchmarkRunner.Run<RngBenchmark>();
+        }
+    }
+}
