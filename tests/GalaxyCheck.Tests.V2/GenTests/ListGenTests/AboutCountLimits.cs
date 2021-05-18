a86e4310b6a69f80d2a0021fb6b3d77c526e9bf4
nth-commit <mfrywhite@gmail.com>
2021-05-18 21:58:25 +1200

    🌍 #216 Add a count limit to Gen.List()

---
+++ tests/GalaxyCheck.Tests.V2/GenTests/ListGenTests/AboutCountLimits.cs
@@ -0,0 +1,59 @@
+﻿using FluentAssertions;
+using GalaxyCheck;
+using NebulaCheck;
+using System;
+using System.Linq;
+using Gen = NebulaCheck.Gen;
+using Property = NebulaCheck.Property;
+using Test = NebulaCheck.Test;
+
+namespace Tests.V2.GenTests.ListGenTests
+{
+    public class AboutCountLimits
+    {
+        [Property]
+        public NebulaCheck.IGen<Test> ItHasACountLimitEnabledByDefault() =>
+            from seed in DomainGen.Seed()
+            from size in DomainGen.Size()
+            from elementGen in DomainGen.Gen()
+            from excessiveCount in Gen.Int32().Between(1001, 2000)
+            select Property.ForThese(() =>
+            {
+                void AssertCountLimit(GalaxyCheck.Gens.IListGen<object> gen)
+                {
+                    Action test = () => gen.SampleOne(seed: seed, size: size);
+
+                    test.Should()
+                        .Throw<GalaxyCheck.Exceptions.GenErrorException>()
+                        .WithMessage(
+                            "Error while running generator ListGen: Count limit exceeded. " +
+                            "This is a built-in safety mechanism to prevent hanging tests. " +
+                            "If generating a list with over 1000 elements was intended, relax this constraint by calling IListGen.DisableCountLimitUnsafe().");
+                }
+
+                AssertCountLimit(elementGen.ListOf().WithCountGreaterThanEqual(excessiveCount));
+                AssertCountLimit(elementGen.ListOf().WithCountLessThanEqual(excessiveCount));
+                AssertCountLimit(elementGen.ListOf().WithCountBetween(excessiveCount, excessiveCount));
+                AssertCountLimit(elementGen.ListOf().WithCount(excessiveCount));
+            });
+
+        [Property(Iterations = 10)]
+        public NebulaCheck.IGen<Test> TheCountLimitCanBeDisabled() =>
+            from seed in DomainGen.Seed()
+            from size in DomainGen.Size()
+            from elementGen in DomainGen.Gen()
+            from excessiveCount in Gen.Int32().Between(1001, 2000)
+            select Property.ForThese(() =>
+            {
+                void AssertCountLimitDisabled(GalaxyCheck.Gens.IListGen<object> gen)
+                {
+                    gen.DisableCountLimitUnsafe().SampleOne(seed: seed, size: size);
+                }
+
+                AssertCountLimitDisabled(elementGen.ListOf().WithCountGreaterThanEqual(excessiveCount));
+                AssertCountLimitDisabled(elementGen.ListOf().WithCountLessThanEqual(excessiveCount));
+                AssertCountLimitDisabled(elementGen.ListOf().WithCountBetween(excessiveCount, excessiveCount));
+                AssertCountLimitDisabled(elementGen.ListOf().WithCount(excessiveCount));
+            });
+    }
+}
