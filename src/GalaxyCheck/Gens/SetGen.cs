98121846a2cebfe767707c237d5b120809a0984b
nth-commit <mfrywhite@gmail.com>
2021-05-23 12:40:30 +1200

    🌍 #163 Add count limit validation to SetGen

--- src/GalaxyCheck/Gens/SetGen.cs
+++ src/GalaxyCheck/Gens/SetGen.cs
@@ -2,11 +2,13 @@
 {
     using GalaxyCheck.Gens;
     using GalaxyCheck.Gens.Internal;
-    using System.Collections.Generic;
 
     public static partial class Gen
     {
-        public static ISetGen<T> Set<T>(IGen<T> elementGen) => new SetGen<T>(RangeIntention.Unspecified());
+        public static ISetGen<T> Set<T>(IGen<T> elementGen) => new SetGen<T>(
+            ElementGen: elementGen,
+            Count: RangeIntention.Unspecified(),
+            EnableCountLimit: null);
     }
 
     public static partial class Extensions
@@ -54,6 +56,7 @@
 namespace GalaxyCheck.Gens
 {
     using GalaxyCheck.Gens.Internal;
+    using GalaxyCheck.Internal;
     using System.Collections.Generic;
 
     public interface ISetGen<T> : IGen<IReadOnlyCollection<T>>
@@ -84,10 +87,30 @@ namespace GalaxyCheck.Gens
         /// </summary>
         /// <returns>A new generator with the biasing effect applied.</returns>
         ISetGen<T> WithCountBias(Gen.Bias bias);
+
+        /// <summary>
+        /// <para>
+        /// By default, trying to generate a set with a count greater than 1000 is disabled. This is a built-in
+        /// safety mechanism to prevent confusing test failures or hangs. Huge sets can potentially blow out the
+        /// performance footprint of a test to unattainable levels, because the generation performance for sets is
+        /// O(n).
+        /// </para>
+        /// <para>
+        /// It's quite easy to accidentally create a set generator for huge sets. For example, the following code
+        /// might otherwise attempt to generate a list with count int.MaxValue:
+        /// <code>
+        /// Gen.Int32().GreaterThanEqual(0).SelectMany(count => Gen.Int32().SetOf().WithCount(count));
+        /// </code>
+        /// </para>
+        /// </summary>
+        /// <returns>A new generator with the limit disabled.</returns>
+        ISetGen<T> DisableCountLimitUnsafe();
     }
 
     internal record SetGen<T>(
-        RangeIntention Count) : GenProvider<IReadOnlyCollection<T>>, ISetGen<T>
+        IGen<T> ElementGen,
+        RangeIntention Count,
+        bool? EnableCountLimit) : GenProvider<IReadOnlyCollection<T>>, ISetGen<T>
     {
         public ISetGen<T> WithCount(int count) => this with { Count = RangeIntention.Exact(count) };
 
@@ -100,38 +123,82 @@ namespace GalaxyCheck.Gens
             throw new System.NotImplementedException();
         }
 
-        protected override IGen<IReadOnlyCollection<T>> Get
-        {
-            get
-            {
-                var countErrorGen = ValidateCount(Count);
+        public ISetGen<T> DisableCountLimitUnsafe() => this with { EnableCountLimit = false };
 
-                if (countErrorGen != null)
+        protected override IGen<IReadOnlyCollection<T>> Get => TryGetCountRange(Count).Match(
+            fromLeft: range => range == null
+                ? GenSetOfUnspecifiedCount(ElementGen, (0, 0), Gen.Bias.WithSize)
+                : GenSetOfSpecifiedCount(ElementGen, range.Value, Gen.Bias.WithSize, EnableCountLimit ?? true),
+            fromRight: gen => gen);
+
+        private static IGen<IReadOnlyCollection<T>> GenSetOfSpecifiedCount(
+            IGen<T> elementGen,
+            (int minCount, int maxCount) range,
+            Gen.Bias bias,
+            bool enableCountLimit)
+        {
+            var (minCount, maxCount) = range;
+
+            if (enableCountLimit && (minCount > 1000 || maxCount > 1000))
+            {
+                return CountLimitError();
+            }
+
+            return Gen.Constant(new List<T>());
+        }
+
+        private static IGen<IReadOnlyCollection<T>> GenSetOfUnspecifiedCount(
+            IGen<T> elementGen,
+            (int minCount, int maxCount) range,
+            Gen.Bias bias)
+        {
+            return Gen.Constant(new List<T>());
+        }
+
+        private static Either<(int minCount, int maxCount)?, IGen<IReadOnlyCollection<T>>> TryGetCountRange(RangeIntention count)
+        {
+            Either<(int minCount, int maxCount)?, IGen<IReadOnlyCollection<T>>> CountError(string message) =>
+                new Right<(int minCount, int maxCount)?, IGen<IReadOnlyCollection<T>>>(Error(message));
+
+            Either<(int minCount, int maxCount)?, IGen<IReadOnlyCollection<T>>> FromUnspecified() =>
+                new Left<(int minCount, int maxCount)?, IGen<IReadOnlyCollection<T>>>(null);
+
+            Either<(int minCount, int maxCount)?, IGen<IReadOnlyCollection<T>>> FromExact(int count) =>
+                count < 0 ? CountError("'count' cannot be negative") : (count, count);
+
+            Either<(int minCount, int maxCount)?, IGen<IReadOnlyCollection<T>>> FromBounded(int? minCount, int? maxCount)
+            {
+                var resolvedMinCount = minCount ?? 0;
+                var resolvedMaxCount = maxCount ?? resolvedMinCount + 20;
+
+                if (resolvedMinCount < 0)
                 {
-                    return countErrorGen;
+                    return CountError("'minCount' cannot be negative");
                 }
 
-                return Gen.Constant(new List<T>());
+                if (resolvedMaxCount < 0)
+                {
+                    return CountError("'maxCount' cannot be negative");
+                }
+
+                if (resolvedMinCount > resolvedMaxCount)
+                {
+                    return CountError("'minCount' cannot be greater than 'maxCount'");
+                }
+
+                return (resolvedMinCount, resolvedMaxCount);
             }
-        }
 
-        private static IGen<IReadOnlyCollection<T>>? ValidateCount(RangeIntention count)
-        {
             return count.Match(
-                onExact: count => ValidateCountNotNegative("count", count),
-                onUnspecified: () => null,
-                onBounded: (minCount, maxCount) =>
-                    (minCount == null ? null : ValidateCountNotNegative("minCount", minCount.Value)) ??
-                    (maxCount == null ? null : ValidateCountNotNegative("maxCount", maxCount.Value)) ??
-                    (minCount != null && maxCount != null && minCount.Value > maxCount.Value
-                        ? Error($"'minCount' cannot be greater than 'maxCount'")
-                        : null));
+                onUnspecified: () => FromUnspecified(),
+                onExact: count => FromExact(count),
+                onBounded: (minCount, maxCount) => FromBounded(minCount, maxCount));
         }
-
-        private static IGen<IReadOnlyCollection<T>>? ValidateCountNotNegative(string paramName, int count) =>
-            count >= 0 ? null : Error($"'{paramName}' cannot be negative");
 
         private static IGen<IReadOnlyCollection<T>> Error(string message) =>
             Gen.Advanced.Error<IReadOnlyCollection<T>>(nameof(SetGen<T>), message);
+
+        private static IGen<IReadOnlyCollection<T>> CountLimitError() =>
+            Error($"Count limit exceeded. This is a built-in safety mechanism to prevent hanging tests. If generating a list with over 1000 elements was intended, relax this constraint by calling {nameof(ISetGen<T>)}.{nameof(ISetGen<T>.DisableCountLimitUnsafe)}().");
     }
 }
