709f77c5347690213ad678f8c483328ed3248901
nth-commit <mfrywhite@gmail.com>
2021-05-24 19:12:47 +1200

    🌍 #163 Generate sets with an unspecified count
    This allows generator expressions like Gen.Boolean().SetOf() to complete. It short-circuits the strategy where we select a count and then generate to that count. This is because we might generate a count bigger than how big the set can possibly be.

---
+++ tests/GalaxyCheck.Tests.V2/GenTests/SetGenTests/AboutExhaustion.cs
@@ -0,0 +1,22 @@
+﻿using FluentAssertions;
+using GalaxyCheck;
+using NebulaCheck;
+using System;
+using static Tests.V2.DomainGenAttributes;
+
+namespace Tests.V2.GenTests.SetGenTests
+{
+    public class AboutExhaustion
+    {
+        [Property(Iterations = 10)]
+        public void ItExhaustsWhenGeneratingASetOfAnImpossibleSize([Seed] int seed, [Size] int size)
+        {
+            var elementGen = GalaxyCheck.Gen.Boolean();
+            var gen = GalaxyCheck.Gen.Set(elementGen).WithCount(3);
+
+            Action test = () => gen.SampleOne(seed: seed, size: size);
+
+            test.Should().Throw<GalaxyCheck.Exceptions.GenExhaustionException>();
+        }
+    }
+}
---
+++ tests/GalaxyCheck.Tests.V2/GenTests/SetGenTests/AboutUnconstrainedGeneration.cs
@@ -0,0 +1,25 @@
+﻿using FluentAssertions;
+using GalaxyCheck;
+using NebulaCheck;
+using System;
+using System.Linq;
+using static Tests.V2.DomainGenAttributes;
+
+namespace Tests.V2.GenTests.SetGenTests
+{
+    public class AboutUnconstrainedGeneration
+    {
+        [Property(Iterations = 10)]
+        public void ItCanGenerateABooleanSet([Seed] int seed, [Size] int size)
+        {
+            var elementGen = GalaxyCheck.Gen.Boolean();
+            var gen = GalaxyCheck.Gen
+                .Set(elementGen)
+                .Where(set => set.Count > 0); // Ensure it doesn't always return an empty set, without explicitly constraining it
+
+            var sample = gen.SampleOneTraversal(seed: seed, size: size);
+
+            sample.Should().OnlyContain(set => set.Distinct().SequenceEqual(set));
+        }
+    }
+}
