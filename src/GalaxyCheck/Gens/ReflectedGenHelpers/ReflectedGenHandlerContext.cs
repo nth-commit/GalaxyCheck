5ae5dd7c7b107c35ebffc57f470d4826b13424f4
nth-commit <mfrywhite@gmail.com>
2021-07-27 09:14:02 +1200

    🌍 #260 Denote strings with quotes in the renderer

---
+++ src/GalaxyCheck/Gens/ReflectedGenHelpers/ReflectedGenHandlerContext.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Immutable;
+
+namespace GalaxyCheck.Gens.ReflectedGenHelpers
+{
+    internal record ReflectedGenHandlerContext(
+        ImmutableList<string> Members,
+        ImmutableStack<Type> TypeHistory)
+    {
+        public static ReflectedGenHandlerContext Create(Type type) =>
+            new ReflectedGenHandlerContext(ImmutableList.Create("$"), ImmutableStack.Create(type));
+
+        public ReflectedGenHandlerContext Next(string memberName, Type type) =>
+            new ReflectedGenHandlerContext(Members.Add(memberName), TypeHistory.Push(type));
+
+        public string MemberPath => string.Join(".", Members);
+    }
+}
