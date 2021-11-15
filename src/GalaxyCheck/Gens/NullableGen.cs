7c4d364b82eb45f44bde1d979df6b6c2e30a48fb
nth-commit <mfrywhite@gmail.com>
2021-11-16 00:34:48 +1300

    🌍 #282 Add Gen.Nullable<T>() and extension variant Gen.OrNull<T>()

---
+++ src/GalaxyCheck/Gens/NullableGen.cs
@@ -0,0 +1,28 @@
+﻿namespace GalaxyCheck
+{
+    public static partial class Gen
+    {
+        /// <summary>
+        /// Generates values from the source generator or nulls, at a ratio of 10:1.
+        /// </summary>
+        /// <param name="gen">The generator to produce the non-nullable values.</param>
+        /// <returns>A new generator.</returns>
+        public static IGen<T?> Nullable<T>(IGen<T> gen)
+            where T : class
+        {
+            return Choose<T?>()
+                .WithChoice(Constant<T?>(default), 1)
+                .WithChoice(gen, 10);
+        }
+    }
+
+    public static partial class Extensions
+    {
+        /// <summary>
+        /// Generates values from the source generator or nulls, at a ratio of 10:1.
+        /// </summary>
+        /// <param name="gen">The generator to produce the non-nullable values.</param>
+        /// <returns>A new generator.</returns>
+        public static IGen<T?> OrNull<T>(this IGen<T> gen) where T : class => Gen.Nullable(gen);
+    }
+}
