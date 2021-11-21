ac35e0f3ae8e45b48389b878545555b6fac836dd
nth-commit <mfrywhite@gmail.com>
2021-11-21 13:57:39 +1300

    🌍 #289 Allow transition behaviour to be decorated, implement resizing as a decorator

---
+++ src/GalaxyCheck/Runners/Check/Automata/CheckStateEnumerator.cs
@@ -0,0 +1,37 @@
+﻿using GalaxyCheck.Internal;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace GalaxyCheck.Runners.Check.Automata
+{
+    internal static class CheckStateEnumerator
+    {
+        public static IEnumerable<CheckStateTransition<T>> Enumerate<T>(
+            CheckState<T> initialState,
+            CheckStateContext<T> initialContext,
+            IReadOnlyCollection<ICheckStateTransitionDecorator<T>> decorators) => EnumerableExtensions.Unfold(
+                new CheckStateTransition<T>(initialState, initialContext),
+                previousTransition =>
+                {
+                    if (previousTransition.State is TerminationState<T>)
+                    {
+                        return new Option.None<CheckStateTransition<T>>();
+                    }
+
+                    var transition = previousTransition.State.Transition(previousTransition.Context);
+
+                    var decoratedTransition = decorators
+                        .Scan(
+                            (previousState: previousTransition.State, nextTransition: transition),
+                            (acc, decorator) =>
+                            {
+                                var nextTransition = decorator.Decorate(acc.previousState, acc.nextTransition);
+                                return (previousState: acc.nextTransition.State, nextTransition);
+                            })
+                        .Select(x => x.nextTransition)
+                        .Last();
+
+                    return new Option.Some<CheckStateTransition<T>>(decoratedTransition);
+                });
+    }
+}
---
+++ src/GalaxyCheck/Runners/Check/Automata/ICheckStateTransitionDecorator.cs
@@ -0,0 +1,9 @@
+﻿namespace GalaxyCheck.Runners.Check.Automata
+{
+    internal interface ICheckStateTransitionDecorator<T>
+    {
+        CheckStateTransition<T> Decorate(
+            CheckState<T> previousState,
+            CheckStateTransition<T> nextTransition);
+    }
+}
