a1d26e1dd8c269c39800ac91c8b11925faff255c
nth-commit <mfrywhite@gmail.com>
2021-11-23 22:17:46 +1300

    🌍 #297 Add roslyn analyzer for checking correctness of a [MemberGen]

---
+++ src/GalaxyCheck.Xunit.Analyzers/Descriptors.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.CodeAnalysis;
+
+namespace GalaxyCheck.Xunit.Analyzers
+{
+    public enum Category
+    {
+        General
+    }
+
+    public static class Descriptors
+    {
+        public readonly static DiagnosticDescriptor GCX1000_MemberGenMustReferenceExistingMember = new DiagnosticDescriptor(
+#pragma warning disable RS2008 // Enable analyzer release tracking
+            "GalaxyCheckXunit1000",
+#pragma warning restore RS2008 // Enable analyzer release tracking
+            "MemberGen must reference an existing member",
+            "MemberData must reference an existing member '{0}' on type '{1}'. Fix the member reference, or add the missing data member.",
+            "General",
+            DiagnosticSeverity.Error,
+            true);
+
+        public static readonly DiagnosticDescriptor GCX1001_MemberDataMustReferenceValidMemberType = new DiagnosticDescriptor(
+#pragma warning disable RS2008 // Enable analyzer release tracking
+            "GalaxyCheckXunit1001",
+#pragma warning restore RS2008 // Enable analyzer release tracking
+            "MemberGen must reference a valid member type",
+            "MemberGen must reference a property or field. Convert the data member to a compatible member type.",
+            "General",
+            DiagnosticSeverity.Error,
+            true);
+
+        public static readonly DiagnosticDescriptor GCX1002_MemberGenMustReferenceMemberOfValidType = new DiagnosticDescriptor(
+#pragma warning disable RS2008 // Enable analyzer release tracking
+            "GalaxyCheckXunit1002",
+#pragma warning restore RS2008 // Enable analyzer release tracking
+            "MemberGen must reference a member providing a suitable data type",
+            "MemberGen must reference a data type assignable to '{0}'. The referenced type '{1}' is not valid.",
+            "General",
+            DiagnosticSeverity.Error,
+            true);
+
+    }
+}
---
+++ src/GalaxyCheck.Xunit.Analyzers/SymbolExtensions.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.CodeAnalysis;
+using System.Linq;
+
+namespace GalaxyCheck.Xunit.Analyzers
+{
+    internal static class SymbolExtensions
+    {
+        public static bool IsAssignableFrom(
+            this ITypeSymbol? targetType,
+            ITypeSymbol? sourceType,
+            bool exactMatch = false)
+        {
+            if (targetType is not null)
+            {
+                while (sourceType is not null)
+                {
+                    if (SymbolEqualityComparer.Default.Equals(sourceType, targetType))
+                        return true;
+
+                    if (exactMatch)
+                        return false;
+
+                    if (targetType.TypeKind == TypeKind.Interface)
+                        return sourceType.AllInterfaces.Any(i => SymbolEqualityComparer.Default.Equals(i, targetType));
+
+                    sourceType = sourceType.BaseType;
+                }
+            }
+
+            return false;
+        }
+    }
+}
