9d722620044d3a58709f3d3e1837aceb6f7e1546
nth-commit <mfrywhite@gmail.com>
2021-11-25 17:49:09 +1300

    🌍 #299 Rename "Analyzers" => "CodeAnalysis"

--- src/GalaxyCheck.Xunit.CodeAnalysis.Vsix/Properties/AssemblyInfo.cs
+++ src/GalaxyCheck.Xunit.CodeAnalysis.Vsix/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("GalaxyCheck.Xunit.Analyzers.Vsix")]
+[assembly: AssemblyTitle("GalaxyCheck.Xunit.CodeAnalysis.Vsix")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("GalaxyCheck.Xunit.Analyzers.Vsix")]
+[assembly: AssemblyProduct("GalaxyCheck.Xunit.CodeAnalysis.Vsix")]
 [assembly: AssemblyCopyright("")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
