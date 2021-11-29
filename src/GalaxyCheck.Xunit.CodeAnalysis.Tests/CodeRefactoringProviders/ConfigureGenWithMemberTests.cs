051939e98d0e99afb1a93994fffd66282b53f315
nth-commit <mfrywhite@gmail.com>
2021-11-29 18:19:54 +1300

    🌍 #298 Basic initial implementation of ConfigureGenWithMember code refactor

---
+++ src/GalaxyCheck.Xunit.CodeAnalysis.Tests/CodeRefactoringProviders/ConfigureGenWithMemberTests.cs
@@ -0,0 +1,63 @@
+﻿using Microsoft.CodeAnalysis.Text;
+using System.Threading.Tasks;
+using Xunit;
+using Verifier = GalaxyCheck.Xunit.CodeAnalysis.Tests.CodeRefactoringProviders.Verifier<GalaxyCheck.Xunit.CodeAnalysis.CodeRefactoringProviders.ConfigureGenWithMember>;
+
+namespace GalaxyCheck.Xunit.CodeAnalysis.Tests.CodeRefactoringProviders
+{
+    /// <summary>
+    /// - Variables:
+    ///     - Line position (anywhere within "int x")
+    ///     - Parameter name/method name, property becomes "[MethodName]_[ParameterName]"
+    /// - Don't add member gen attribute if it already exists
+    /// - Handle Lists with explicit generators, and other higher-order generators (nullable etc.)
+    /// - Provide the refactor anywhere inside the method declaration node, having it provide an option for each argument
+    /// </summary>
+    public class ConfigureGenWithMemberTests
+    {
+        [Theory]
+        [InlineData("byte", "Gen.Byte()")]
+        [InlineData("short", "Gen.Int16()")]
+        [InlineData("int", "Gen.Int32()")]
+        [InlineData("long", "Gen.Int64()")]
+        [InlineData("string", "Gen.String()")]
+        [InlineData("char", "Gen.Char()")]
+        [InlineData("bool", "Gen.Boolean()")]
+        [InlineData("System.Guid", "Gen.Guid()")]
+        [InlineData("System.DateTime", "Gen.DateTime()")]
+        [InlineData("TestClass", "Gen.Create<TestClass>()")] // A random type, not generatable by default, which is in scope
+        public async Task ItInsertsAndReferencesTheMember(string type, string genExpression)
+        {
+            var code = @"
+using GalaxyCheck;
+
+public class TestClass
+{
+    [Property]
+    public void TestMethod(" + type + @" x)
+    {
+    }
+}
+";
+            var codeProvidingRefactorPosition = new LinePosition(7, 28);
+
+            var expectedRefactoredCode = @"
+using GalaxyCheck;
+
+public class TestClass
+{
+    private static IGen<" + type + @"> TestMethod_x => " + genExpression + @";
+
+    [Property]
+    public void TestMethod([MemberGen(nameof(TestMethod_x))] " + type + @" x)
+    {
+    }
+}
+";
+
+            var expectedRefactoringTitle = $"Configure generation of parameter with MemberGenAttribute";
+
+            await Verifier.Verify(code, codeProvidingRefactorPosition, expectedRefactoredCode, expectedRefactoringTitle);
+        }
+    }
+}
---
+++ src/GalaxyCheck.Xunit.CodeAnalysis.Tests/CodeRefactoringProviders/Verifier.cs
@@ -0,0 +1,62 @@
+﻿using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.CodeRefactorings;
+using Microsoft.CodeAnalysis.CSharp.Testing;
+using Microsoft.CodeAnalysis.Testing;
+using Microsoft.CodeAnalysis.Testing.Verifiers;
+using Microsoft.CodeAnalysis.Text;
+using System.Linq;
+using System.Reflection;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace GalaxyCheck.Xunit.CodeAnalysis.Tests.CodeRefactoringProviders
+{
+    public static class Verifier<TCodeRefactoringProvider>
+        where TCodeRefactoringProvider : CodeRefactoringProvider, new()
+    {
+        public static Task Verify(
+            string code,
+            string codeProvidingRefactor,
+            string expectedRefactoredCode,
+            string expectedRefactoringTitle)
+        {
+            var linePosition = new LinePosition(1, 1);
+
+            var indexOfCodeRefactoringProvider = code.IndexOf(codeProvidingRefactor);
+
+
+            return Verify(code, linePosition, expectedRefactoredCode, expectedRefactoringTitle);
+        }
+
+        public static async Task Verify(
+            string code,
+            LinePosition codeProvidingRefactorPosition,
+            string expectedRefactoredCode,
+            string expectedRefactoringTitle)
+        {
+            var test = new CSharpCodeRefactoringTest<TCodeRefactoringProvider, XUnitVerifier>()
+            {
+                TestCode = code,
+                FixedCode = expectedRefactoredCode,
+                CodeActionVerifier = (codeAction, verifier) =>
+                {
+                    verifier.Equal(codeAction.Title, expectedRefactoringTitle);
+                }
+            };
+
+            test.ExpectedDiagnostics.Add(new DiagnosticResult("Refactoring", DiagnosticSeverity.Hidden)
+                .WithSpan(codeProvidingRefactorPosition.Line, codeProvidingRefactorPosition.Character, codeProvidingRefactorPosition.Line, codeProvidingRefactorPosition.Character));
+
+            test.OffersEmptyRefactoring = true;
+
+            test.TestState.AdditionalReferences.AddRange(new[]
+            {
+                MetadataReference.CreateFromFile(Assembly.GetAssembly(typeof(FactAttribute))!.Location),
+                MetadataReference.CreateFromFile(Assembly.GetAssembly(typeof(IGen))!.Location),
+                MetadataReference.CreateFromFile(Assembly.GetAssembly(typeof(PropertyAttribute))!.Location)
+            });
+
+            await test.RunAsync();
+        }
+    }
+}
