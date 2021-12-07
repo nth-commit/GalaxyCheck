c803d14581a06b54b92da8441d3a6cdeb1e61ab8
nth-commit <mfrywhite@gmail.com>
2021-12-08 08:20:53 +1300

    🌍 Update to pre-release packages

--- src/GalaxyCheck.Tests/GenTests/ByteGenTests/AboutDefaults.cs
+++ src/GalaxyCheck.Tests/GenTests/ByteGenTests/AboutDefaults.cs
@@ -1,83 +1,67 @@
 ﻿using FluentAssertions;
-using GalaxyCheck;
 using NebulaCheck;
 using System.Collections.Generic;
-using System.Linq;
-using Gen = NebulaCheck.Gen;
+using static Tests.V2.DomainGenAttributes;
 using Property = NebulaCheck.Property;
-using Test = NebulaCheck.Test;
 
 namespace Tests.V2.GenTests.ByteGenTests
 {
     public class AboutDefaults
     {
         [Property]
-        public NebulaCheck.IGen<Test> TheDefaultMinimumIsTheMinimumByte() =>
-            from seed in DomainGen.Seed()
-            from size in DomainGen.Size()
-            select Property.ForThese(() =>
-            {
-                List<byte> SampleTraversal(GalaxyCheck.IGen<byte> gen) => gen.SampleOneTraversal(seed: seed, size: size);
+        public void TheDefaultMinimumIsTheMinimumByte([Seed] int seed, [Size] int size)
+        {
+            List<byte> SampleTraversal(GalaxyCheck.IGen<byte> gen) => gen.SampleOneTraversal(seed: seed, size: size);
 
-                var gen0 = GalaxyCheck.Gen.Byte();
-                var gen1 = gen0.GreaterThanEqual(byte.MinValue);
+            var gen0 = GalaxyCheck.Gen.Byte();
+            var gen1 = gen0.GreaterThanEqual(byte.MinValue);
 
-                var sample0 = SampleTraversal(gen0);
-                var sample1 = SampleTraversal(gen1);
+            var sample0 = SampleTraversal(gen0);
+            var sample1 = SampleTraversal(gen1);
 
-                sample0.Should().BeEquivalentTo(sample1);
-            });
+            sample0.Should().BeEquivalentTo(sample1);
+        }
 
         [Property]
-        public NebulaCheck.IGen<Test> TheDefaultMaximumIsTheMaximumByte() =>
-            from seed in DomainGen.Seed()
-            from size in DomainGen.Size()
-            select Property.ForThese(() =>
-            {
-                List<byte> SampleTraversal(GalaxyCheck.IGen<byte> gen) => gen.SampleOneTraversal(seed: seed, size: size);
+        public void TheDefaultMaximumIsTheMaximumByte([Seed] int seed, [Size] int size)
+        {
+            List<byte> SampleTraversal(GalaxyCheck.IGen<byte> gen) => gen.SampleOneTraversal(seed: seed, size: size);
 
-                var gen0 = GalaxyCheck.Gen.Byte();
-                var gen1 = gen0.LessThanEqual(byte.MaxValue);
+            var gen0 = GalaxyCheck.Gen.Byte();
+            var gen1 = gen0.LessThanEqual(byte.MaxValue);
 
-                var sample0 = SampleTraversal(gen0);
-                var sample1 = SampleTraversal(gen1);
+            var sample0 = SampleTraversal(gen0);
+            var sample1 = SampleTraversal(gen1);
 
-                sample0.Should().BeEquivalentTo(sample1);
-            });
+            sample0.Should().BeEquivalentTo(sample1);
+        }
 
         [Property]
-        public NebulaCheck.IGen<Test> TheDefaultOriginIsTheMinimum() =>
-            from seed in DomainGen.Seed()
-            from size in DomainGen.Size()
-            from min in Gen.Byte()
-            select Property.ForThese(() =>
-            {
-                List<byte> SampleTraversal(GalaxyCheck.IGen<byte> gen) => gen.SampleOneTraversal(seed: seed, size: size);
+        public void TheDefaultOriginIsTheMinimum([Seed] int seed, [Size] int size, byte min)
+        {
+            List<byte> SampleTraversal(GalaxyCheck.IGen<byte> gen) => gen.SampleOneTraversal(seed: seed, size: size);
 
-                var gen0 = GalaxyCheck.Gen.Byte().GreaterThanEqual(min);
-                var gen1 = gen0.ShrinkTowards(min);
+            var gen0 = GalaxyCheck.Gen.Byte().GreaterThanEqual(min);
+            var gen1 = gen0.ShrinkTowards(min);
 
-                var sample0 = SampleTraversal(gen0);
-                var sample1 = SampleTraversal(gen1);
+            var sample0 = SampleTraversal(gen0);
+            var sample1 = SampleTraversal(gen1);
 
-                sample0.Should().BeEquivalentTo(sample1);
-            });
+            sample0.Should().BeEquivalentTo(sample1);
+        }
 
         [Property]
-        public NebulaCheck.IGen<Test> TheDefaultBiasIsWithSize() =>
-            from seed in DomainGen.Seed()
-            from size in DomainGen.Size()
-            select Property.ForThese(() =>
-            {
-                List<byte> SampleTraversal(GalaxyCheck.IGen<byte> gen) => gen.SampleOneTraversal(seed: seed, size: size);
+        public void TheDefaultBiasIsWithSize([Seed] int seed, [Size] int size)
+        {
+            List<byte> SampleTraversal(GalaxyCheck.IGen<byte> gen) => gen.SampleOneTraversal(seed: seed, size: size);
 
-                var gen0 = GalaxyCheck.Gen.Byte();
-                var gen1 = gen0.WithBias(GalaxyCheck.Gen.Bias.WithSize);
+            var gen0 = GalaxyCheck.Gen.Byte();
+            var gen1 = gen0.WithBias(GalaxyCheck.Gen.Bias.WithSize);
 
-                var sample0 = SampleTraversal(gen0);
-                var sample1 = SampleTraversal(gen1);
+            var sample0 = SampleTraversal(gen0);
+            var sample1 = SampleTraversal(gen1);
 
-                sample0.Should().BeEquivalentTo(sample1);
-            });
+            sample0.Should().BeEquivalentTo(sample1);
+        }
     }
 }
