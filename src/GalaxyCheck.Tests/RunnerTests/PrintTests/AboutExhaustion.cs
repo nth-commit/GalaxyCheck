b2a95b2f4ee62978dd13a1902c93243a995cf386
nth-commit <mfrywhite@gmail.com>
2022-07-17 10:15:01 +1200

    🌍 Add exhaustion to `CheckAsync`

--- src/GalaxyCheck.Tests/RunnerTests/PrintTests/AboutExhaustion.cs
+++ src/GalaxyCheck.Tests/RunnerTests/PrintTests/AboutExhaustion.cs
@@ -3,20 +3,48 @@ using GalaxyCheck;
 using NebulaCheck;
 using System;
 using System.Linq;
+using System.Threading.Tasks;
 using static Tests.V2.DomainGenAttributes;
+using static Tests.V2.Timeouts;
 
 namespace Tests.V2.RunnerTests.PrintTests
 {
     public class AboutExhaustion
     {
-        [Property(Iterations = 1)]
-        public void ItCanExhaust([Seed] int seed, [Size] int size)
+        public class Sync
         {
-            var property = GalaxyCheck.Gen.Int32().Where(x => false).ForAll(_ => true);
+            [Property(Iterations = 1)]
+            public void ItCanExhaust([Seed] int seed, [Size] int size)
+            {
+                RunWithTimeout(
+                    () =>
+                    {
+                        var property = GalaxyCheck.Gen.Int32().Where(x => false).ForAll(_ => true);
 
-            Action test = () => property.Advanced.Print(seed: seed, size: size);
+                        Action test = () => property.Print(seed: seed, size: size);
 
-            test.Should().Throw<GalaxyCheck.Exceptions.GenExhaustionException>();
+                        test.Should().Throw<GalaxyCheck.Exceptions.GenExhaustionException>();
+                    },
+                    TimeSpan.FromSeconds(20));
+            }
+        }
+
+        public class Async
+        {
+            [Property(Iterations = 1)]
+            public void ItCanExhaustAsync([Seed] int seed, [Size] int size)
+            {
+                RunWithTimeoutAsync(
+                    async () =>
+                    {
+                        var property = GalaxyCheck.Gen.Int32().Where(x => false).ForAllAsync(_ => Task.FromResult(true));
+
+                        Func<Task> test = () => property.PrintAsync(seed: seed, size: size);
+
+                        await test.Should().ThrowAsync<GalaxyCheck.Exceptions.GenExhaustionException>();
+                    },
+                    TimeSpan.FromSeconds(20));
+            }
         }
     }
 }
--- src/GalaxyCheck/Internal/AsyncEnumerableExtensions.cs
+++ src/GalaxyCheck/Internal/AsyncEnumerableExtensions.cs
@@ -42,6 +42,62 @@ namespace GalaxyCheck.Internal
             }
         }
 
+        public static async IAsyncEnumerable<(TSource element, TAccumulator state)> ScanInParallel<TSource, TAccumulator>(
+            this IAsyncEnumerable<TSource> input,
+            TAccumulator seed,
+            Func<TAccumulator, TSource, TAccumulator> next)
+        {
+            var state = seed;
+            await foreach (var item in input)
+            {
+                state = next(state, item);
+                yield return (item, state);
+            }
+        }
+
+        public static IAsyncEnumerable<(T element, int consecutiveDiscardCount)> WithConsecutiveDiscardCount<T>(
+            this IAsyncEnumerable<T> source,
+            Func<T, bool> isCounted,
+            Func<T, bool> isDiscard)
+        {
+            return source.ScanInParallel(0, (consecutiveDiscardCount, element) =>
+            {
+                if (isCounted(element) == false)
+                {
+                    // The element is considered neither a discard or a non-discard, don't increment or reset the
+                    // consecutive discard count.
+                    return consecutiveDiscardCount;
+                }
+
+                if (isDiscard(element))
+                {
+                    return consecutiveDiscardCount + 1;
+                }
+
+                return 0;
+            });
+        }
+
+        public static IAsyncEnumerable<T> WithDiscardCircuitBreaker<T>(
+            this IAsyncEnumerable<T> source,
+            Func<T, bool> isCounted,
+            Func<T, bool> isDiscard)
+        {
+            return source
+                .WithConsecutiveDiscardCount(isCounted, isDiscard)
+                .Select((x) =>
+                {
+                    var (element, consecutiveDiscardCount) = x;
+
+                    if (consecutiveDiscardCount > 500)
+                    {
+                        throw new Exceptions.GenExhaustionException();
+                    }
+
+                    return element;
+                });
+        }
+
         public static async Task<(T? head, IAsyncEnumerable<T> tail)> Deconstruct<T>(this IAsyncEnumerable<T> source)
         {
             var head = await source.FirstOrDefaultAsync();
