89014ca2db22f2e499662acb8089a0cc12b58874
nth-commit <mfrywhite@gmail.com>
2022-08-02 20:43:12 +1200

    🌍 #343 Support generating nullable structs via reflection!

---
+++ src/GalaxyCheck/Gens/ReflectedGenHelpers/ReflectedGenHandlers/NullableGenHandler.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GalaxyCheck.Gens.ReflectedGenHelpers.ReflectedGenHandlers
+{
+    internal class NullableGenHandler : IReflectedGenHandler
+    {
+        public bool CanHandleGen(Type type, ReflectedGenHandlerContext context) => IsNullableStruct(type);
+
+        public IGen CreateGen(IReflectedGenHandler innerHandler, Type type, ReflectedGenHandlerContext context)
+        {
+            if (IsNullableStruct(type))
+            {
+                var nonNullType = type.GetGenericArguments().Single();
+
+                var innerGen = innerHandler.CreateGen(nonNullType, context);
+
+                var methodInfo = typeof(Gen).GetMethod(
+                    nameof(Gen.NullableStruct),
+                    BindingFlags.Static | BindingFlags.Public)!;
+                var genericMethodInfo = methodInfo.MakeGenericMethod(nonNullType);
+
+                return (IGen)genericMethodInfo.Invoke(null!, new object[] { innerGen })!;
+            }
+
+            throw new NotImplementedException();
+        }
+
+        private static bool IsNullableStruct(Type type)
+        {
+            if (type.IsGenericType)
+            {
+                var genericTypeDefinition = type.GetGenericTypeDefinition();
+                return genericTypeDefinition == typeof(Nullable<>);
+            }
+
+            return false;
+        }
+    }
+}
