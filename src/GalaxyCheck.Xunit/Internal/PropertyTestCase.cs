140924c1b4d668a455cfec4a1acf7483bc170b03
nth-commit <mfrywhite@gmail.com>
2022-08-07 15:57:27 +1200

    🌍 #352 Return execution time in run summary

--- src/GalaxyCheck.Xunit/Internal/PropertyTestCase.cs
+++ src/GalaxyCheck.Xunit/Internal/PropertyTestCase.cs
@@ -37,30 +37,32 @@ namespace GalaxyCheck.Xunit.Internal
             messageBus.QueueMessage(new TestCaseStarting(this));
 
             var startTime = DateTime.UtcNow;
+
             var test = new XunitTest(this, DisplayName);
             var testOutputHelper = constructorArguments
                 .OfType<TestOutputHelper>()
                 .FirstOrDefault() ?? new TestOutputHelper();
             testOutputHelper.Initialize(messageBus, test);
 
-            Task<RunSummary> Fail(Exception ex)
+            RunSummary Fail(Exception ex)
             {
                 var executionTime = (decimal)((DateTime.UtcNow - startTime).TotalSeconds);
                 messageBus.QueueMessage(new TestFailed(test, executionTime, testOutputHelper!.Output, ex));
-                return Task.FromResult(new RunSummary() { Failed = 1 });
+                return new RunSummary() { Failed = 1, Total = 1, Skipped = 0, Time = executionTime  };
             }
 
-            Task<RunSummary> Pass()
+            RunSummary Pass()
             {
                 var executionTime = (decimal)((DateTime.UtcNow - startTime).TotalSeconds);
                 messageBus.QueueMessage(new TestPassed(test, executionTime, testOutputHelper!.Output));
-                return Task.FromResult(new RunSummary() { Total = 1 });
+                return new RunSummary() { Failed = 0, Total = 1, Skipped = 0, Time = executionTime };
             }
 
-            Task<RunSummary> Skip(string reason)
+            RunSummary Skip(string reason)
             {
+                var executionTime = (decimal)((DateTime.UtcNow - startTime).TotalSeconds);
                 messageBus.QueueMessage(new TestSkipped(test, reason));
-                return Task.FromResult(new RunSummary() { Skipped = 1 });
+                return new RunSummary() { Failed = 0, Total = 0, Skipped = 1, Time = executionTime };
             }
 
             PropertyInitializationResult? propertyInitResult;
@@ -74,22 +76,22 @@ namespace GalaxyCheck.Xunit.Internal
             }
             catch (Exception exception)
             {
-                return await Fail(exception);
+                return Fail(exception);
             }
 
             if (propertyInitResult.ShouldSkip)
             {
-                return await Skip(propertyInitResult.SkipReason!);
+                return Skip(propertyInitResult.SkipReason!);
             }
 
             try
             {
                 await propertyInitResult.Runner.Run(propertyInitResult.Parameters, testOutputHelper);
-                return await Pass();
+                return Pass();
             }
             catch (Exception propertyFailedException)
             {
-                return await Fail(propertyFailedException);
+                return Fail(propertyFailedException);
             }
         }
     }
