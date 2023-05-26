34d47a4d733bda03377b56e9573df5010e3ce0bd
nth-commit <mfrywhite@gmail.com>
2023-05-26 18:02:34 +1200

    📖 Sample: "control" data and dynamic generation

---
+++ samples/GalaxyCheck.Samples.EverythingIsSerializable/XunitExtensions.cs
@@ -0,0 +1,18 @@
+﻿using Xunit;
+
+namespace GalaxyCheck.Samples.EverythingIsSerializable;
+
+public static class XunitExtensions
+{
+    public static TheoryData<T> ToTheoryData<T>(this IEnumerable<T> enumerable)
+    {
+        var theoryData = new TheoryData<T>();
+
+        foreach (var item in enumerable)
+        {
+            theoryData.Add(item);
+        }
+
+        return theoryData;
+    }
+}
