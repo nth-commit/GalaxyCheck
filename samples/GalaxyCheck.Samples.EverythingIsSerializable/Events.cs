228d6370f69a43695755def34f6047acee153889
nth-commit <mfrywhite@gmail.com>
2023-05-26 18:02:34 +1200

    📖 Sample: "control" data and dynamic generation

---
+++ samples/GalaxyCheck.Samples.EverythingIsSerializable/Events.cs
@@ -0,0 +1,24 @@
+﻿namespace GalaxyCheck.Samples.EverythingIsSerializable;
+
+public static class Events
+{
+    public interface IEvent
+    {
+        string Type { get; }
+    }
+
+    public record InvoiceCreatedEvent(string SupplierId, string CustomerId, string InvoiceId) : IEvent
+    {
+        public string Type => "InvoiceCreatedEvent";
+    }
+
+    public record InvoiceUpdatedEvent(string SupplierId, string InvoiceId) : IEvent
+    {
+        public string Type => "InvoiceUpdatedEvent";
+    }
+
+    public record InvoicePaidEvent(string SupplierId, string InvoiceId, decimal PaidAmount) : IEvent
+    {
+        public string Type => "InvoicePaidEvent";
+    }
+}
